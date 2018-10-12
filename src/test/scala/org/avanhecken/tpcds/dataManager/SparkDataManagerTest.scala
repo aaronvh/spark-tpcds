94eb1692dd11691b823ca05f2e94fdd115cf3020
Aäron Van Hecken <aaronvanhecken@localhost.localdomain>
2018-10-12 23:23:35 +0200

    Added test for two runs and get df

--- src/test/scala/org/avanhecken/tpcds/dataManager/SparkDataManagerTest.scala
+++ src/test/scala/org/avanhecken/tpcds/dataManager/SparkDataManagerTest.scala
@@ -18,4 +18,21 @@ class SparkDataManagerTest extends SparkFlatSpec with Matchers {
       .agg(sum('elapsed_time))
       .show(false)
   }
+
+  it should "list two runs in one dataframe" in {
+    SparkTPCDS.main(Array("execute", "test1", "Test if query 1 runs successfully", "tpcds", resourceLocation, "1"))
+    SparkTPCDS.main(Array("execute", "test2", "Test if query 1 runs successfully", "tpcds", resourceLocation, "1,2"))
+
+    val df1 = dataManager.getDF("test1")
+    val df2 = dataManager.getDF("test2")
+
+    val df = df1.union(df2)
+
+    import spark.implicits._
+    df
+      .groupBy('run)
+      .pivot("query")
+      .agg(sum('elapsed_time))
+      .show(false)
+  }
 }
